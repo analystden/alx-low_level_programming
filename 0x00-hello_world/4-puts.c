0851f5b1afeb132d5ffdb22c6d48fcfde80ed5f1
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-08 15:45:00 -0700

    C program using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main = Entry point
+ * main - Entry point
  *
- * Description: A program that prints exactly "Programming is like building a multilingual puzzle"
+ * Description: A program that prints exactly "Programming is like building a multilingual puzzle
  *
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
