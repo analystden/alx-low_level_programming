9009d186cd3fa0ca5682e884c4b841995fba08a3
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-09 03:32:22 -0700

    Sizeof to print bytes of various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 {
 	/*sizeof is used to determine the sizes of various data types*/
 
-	printf("Size of a char: %lu byes\n", sizeof(char));
-	printf("Size of an int: %lu bytes\n", sizeof(int));
-	printf("Size of a long int: %lu bytes\n", sizeof(long int));
-	printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
