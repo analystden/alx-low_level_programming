865cbca8cade4ff9e89cd238426a20ca2b8ac2de
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-10 09:56:29 -0700

    Prints without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unitstd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote but not with any of the printfs or puts.
+ *
+ * Return: It should return 1
+ *
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+
+}
