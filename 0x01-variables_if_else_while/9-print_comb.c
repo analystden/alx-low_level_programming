54aafad9307ca8ac8e421a94e86950c203b4a8f0
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-12 01:00:29 -0700

    Prints numbers separated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Descripttion: prints single digits separated by comma
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0, i < 9, i++)
+	{
+		putchar('0' + i);
+		if (i < 9)
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
