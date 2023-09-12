a7118525a39477085d02e41aebe1b4c1b114ee41
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-12 13:47:15 -0700

    Prints two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Descripttion: prints single digits separated by comma
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+int main(void)
+{
+	int i = 0, j = 0;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <= 9; j++)
+	{
+		{
+		putchar(i + '0');
+		putchar(j + '0');
+
+		if (i != 9 || j != 9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
