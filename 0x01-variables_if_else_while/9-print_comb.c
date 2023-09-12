62c077bada68fcb70bcdd2b59db145fbb22c8310
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-12 01:05:11 -0700

    Prints numbers separated by comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
-		if (i <= 9)
+		if (i < 10)
 		putchar(',');
 		putchar(' ');
 	}
