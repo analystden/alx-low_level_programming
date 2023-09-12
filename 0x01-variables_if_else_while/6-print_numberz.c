6ab0ac8620cad5601cd5aebcd4d132376be8211f
Dennis Kojo Ayertey <denkayer@gmail.com>
2023-09-11 23:50:11 -0700

    Print numbers of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		++digit;
+	}
+
+	putchar("\n");
+
+	return (0);
+}
